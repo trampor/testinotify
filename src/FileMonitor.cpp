22477a9ae8230301cc4d0222bd618ee78c063b72
trampor <xjxing@cti.com.cn>
2016-12-05 16:08:33 +0800

    adsf

--- src/FileMonitor.cpp
+++ src/FileMonitor.cpp
@@ -377,9 +377,6 @@ void* FileMonitor::ImpWorkThread()
 					if(pevent->mask & IN_ISDIR)
 					{
 						cout << "recv a dir IN_ATTRIB notify : "<< pevent->wd  <<" "<< pevent->name << endl;
-						cout << "recv a dir IN_ATTRIB notify : "<< pevent->wd  <<" "<< pevent->name << endl;
-						cout << "recv a dir IN_ATTRIB notify : "<< pevent->wd  <<" "<< pevent->name << endl;
-
 					}
 					else
 					{
